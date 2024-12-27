0682412ed61a9980143fa5230d3c00a904bbf86b
drevsell <drevsell@gmail.com>
2024-12-27 21:22:40 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url = "http://localhost:8000/";
+export const server = "https://test1betry5.vercel.app/api/v2";
+export const backend_url = "https://test1betry5.vercel.app/";
 
 
