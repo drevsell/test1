0a71b17ac4c4d8cac2ff3126b9407bfc0d74de86
drevsell <drevsell@gmail.com>
2024-12-27 21:17:25 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://test1-3596.vercel.app',
   credentials: true
 }));
 
